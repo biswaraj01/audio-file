73fba94d3f26ed034fe01844d7ba65d9e442afe2
biswaraj01 <biswaraj.majumder@acceleronsolutions.io>
2024-08-28 11:05:54 +0530

    Update qrcodegen.py

--- qrcodegen.py
+++ qrcodegen.py
@@ -2,7 +2,7 @@ import qrcode
 
 features = qrcode.QRCode(version = 1, box_size = 30, border = 3)
 
-features.add_data("https://github.com/biswaraj01/audio-file/raw/main/voice2.mp3")
+features.add_data("https://github.com/biswaraj01/audio-file/raw/main/voice.mp3")
 features.make(fit=True)
 generate_image = features.make_image(fill_color = 'black', bg = 'white')
 generate_image.save('qrcode.png')
